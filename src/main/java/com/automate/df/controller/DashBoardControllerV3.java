fdba503c412956994a4b1fc3b0e778208b33556b
SusmithaKonatham <78208308+SusmithaKonatham@users.noreply.github.com>
2022-05-18 18:23:34 +0530

    DmsTargetParamSchedular API
    DmsTargetParamSchedular API

---
+++ src/main/java/com/automate/df/controller/DashBoardControllerV3.java
@@ -0,0 +1,46 @@
+package com.automate.df.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.automate.df.entity.DmsTargetParamSchedular;
+import com.automate.df.exception.DynamicFormsServiceException;
+import com.automate.df.model.df.dashboard.DashBoardReqV3;
+import com.automate.df.service.DashBoardServiceV3;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+@RestController
+@Slf4j
+@Api(value = "/dashboard", tags = "dashboard V2", description = "dashboard V2")
+@RequestMapping(value="/dashboard")
+public class DashBoardControllerV3 {
+	@Autowired
+	Environment env;
+	
+
+	@Autowired
+	DashBoardServiceV3 dashBoardService;
+	@CrossOrigin
+	@PostMapping(value = "v2/get_target_params_scheduler")
+	public ResponseEntity< List<DmsTargetParamSchedular>> getTargetAchivementParams(@RequestBody DashBoardReqV3 req)
+			throws DynamicFormsServiceException {
+		 List<DmsTargetParamSchedular>  response = null;
+		if (Optional.of(req).isPresent()) {
+			response = dashBoardService.getTargetAchivementParams(req.getEmpId());
+		} else {
+			throw new DynamicFormsServiceException(env.getProperty("BAD_REQUEST"), HttpStatus.BAD_REQUEST);
+		}
+		return new ResponseEntity<>(response, HttpStatus.OK);
+	}
+}
