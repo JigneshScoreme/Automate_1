34221116c60ab35b89ad1f8087bc10da95cfcd21
srujanch <srujankrishnach@yahoo.com>
2022-06-28 19:16:04 +0530

    Added supproting params

--- src/main/java/com/automate/df/dao/DmsDeliveryDao.java
+++ src/main/java/com/automate/df/dao/DmsDeliveryDao.java
@@ -16,5 +16,11 @@ public interface DmsDeliveryDao extends JpaRepository<DmsDelivery, Integer>{
 	@Query(value="select  * from dms_delivery where lead_id = :id",nativeQuery = true)
 	public List<DmsDelivery> getDeliveriesWithLeadId(@Param(value="id") int leadId);
 
+	@Query(value="SELECT insurance_taken FROM dms_delivery where lead_id in (:leadList) and insurance_taken=:type",nativeQuery = true)
+	public List<String> getInsuranceTakenLeads(@Param(value="leadList") List<Integer> collect, @Param(value="type") String insuranceType);
+
+	@Query(value="SELECT warranty_taken FROM dms_delivery where lead_id in (:leadList) and warranty_taken='YES'",nativeQuery = true)
+	public List<String> getWarrantyTakenLeads(@Param(value="leadList")  List<Integer> leadIdList);
+
 
 }
